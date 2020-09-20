68a7397486dd0fce18ff24fcf0c9a761535cf31f
m123o <m123oo@naver.com>
2020-09-21 04:05:02 +0900

    added pred

---
+++ sizoo_proApp/predict/predict.py
@@ -0,0 +1,81 @@
+from ..models import LineUp, ShoesData, ShoesExp, UserInfo, ServiceResult
+
+def predict(user, tgt):
+
+    #get tgt lineup
+    tgt_data = ShoesData.objects.get(id=tgt)
+    tgt_lineup = tgt_data.Model_lineUp.pk
+    tgt_brand = LineUp.objects.get(id=tgt_lineup).LineUp_Brand
+
+    #get user shoe exp 
+    user_shoe_query = ShoesExp.objects.filter(ShoesExp_User = user).select_related('LineUp').values('ShoesExp_Size','LineUp_Model_Code','LineUp__pk')
+    user_shoe_sizes = {}
+    
+    user_shoe_pk_list = []
+
+
+    for i in user_shoe_query:
+        if i['LineUp_Model_Code'] not in user_shoe_sizes:
+            user_shoe_sizes[i['LineUp_Model_Code']] = [i['ShoesExp_Size']]
+        else :
+            user_shoe_sizes[i['LineUp_Model_Code']].append(i['ShoesExp_Size'])
+        if i['LineUp__pk'] in user_shoe_pk_list:
+            user_shoe_pk_list.append(i['LineUp__pk'])
+
+    user_shoe_list = list(user_shoe_sizes.keys())
+
+    for i in user_shoe_list:
+        if len(user_shoe_sizes[i]) == 1:
+            user_shoe_sizes[i] = user_shoe_sizes[i][0]
+        else :
+            user_shoe_sizes[i] = int(sum(user_shoe_sizes[i])/len(user_shoe_sizes[i]))
+    
+    #Sol 1
+    # tgt_lineup_models = tgt_data.Line_Up_models.all().values('id')
+    # tgt_lineup_models = [i['id'] for i in tgt_lineup_models]
+
+    ref_users = ShoesExp.select_related('ShoeExp_Shoe').filter(Model_lineUp = tgt_data.id).values_list('ShoesExp_User',flat=True)
+
+    ref_users_pks =[]
+    ref_users_sizes = []
+    ref_count = []
+    for i in ref_users:
+        q = ShoesExp.objects.filter(ShoesExp_User=i).select_related('ShoesExp_Shoe').filter(Model_lineUp=user_shoe_pk_list)
+        if len(q) > 0:
+            t={}
+            for j in q:
+                t[j.LineUp_Model_Code] = j.ShoesExp_Size
+            ref_users_pks.append(i)
+            ref_users_sizes.append(t)
+            ref_count.append(len(t.keys()))
+    
+    adjvallist = []
+    for i in ref_users_sizes:
+        keys = list(i.keys())
+        t = 0
+        for j in keys:
+            t = meanVauser_shoe_sizes[j]-i[j]
+        t /= len(keys)
+
+        adjvallist.append(t)
+
+    mxcnt = max(ref_count)
+    if mxcnt > 0:
+        ref_sizes = [i for i,val in enumerate(l) if val==mxcnt]
+
+    result = 0
+
+    for i in ref_sizes:
+        q = ShoesExp.objects.filter(ShoesExp_User = ref_users_pks[i]).select_related('ShoesExp_shoe').filter(Model_lineUp=tgt_lineup)
+        t=0
+        for j in q:
+            t+=j.ShoesExp_Size
+        t/=len(q)
+        result += t + adjvallist[i]
+    result/=len(ref_sizes)
+    adj = result %5
+    result = (result//5)*5
+    if adj >= 3:
+        result+=5
+
+    return result
